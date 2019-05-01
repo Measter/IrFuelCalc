7a8a433fd44c265504e74e138c3f46e9d91280e8
Stuart Haidon <serayen.sh@gmail.com>
2019-05-01 17:05:31 +0100

    Attempt to take into account current lap progress for total fuel required.

--- IrFuelCalc/Form1.cs
+++ IrFuelCalc/Form1.cs
@@ -172,7 +172,7 @@ namespace IrFuelCalc
 
             m_lapsRemaining = Math.Min( m_estimatedLaps, m_telemLaps );
 
-            m_totalFuelRequired = avgFuel * ( m_lapsRemaining + (int) nudLapOffset.Value ) - e.TelemetryInfo.FuelLevel.Value;
+            m_totalFuelRequired = avgFuel * ( m_lapsRemaining - e.TelemetryInfo.LapDistPct.Value + (int) nudLapOffset.Value ) - e.TelemetryInfo.FuelLevel.Value;
 
             if( m_totalFuelRequired > 0.0 )
             {
